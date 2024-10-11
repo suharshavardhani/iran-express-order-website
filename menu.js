67400036a2b0d1c8b11af8122039a3f172df9867
suharshavardhani <34936342+suharshavardhani@users.noreply.github.com>
2024-10-11 16:32:04 -0700

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-const menu = [
-  { id: 1, name: "Garlic Bread", category: "Starters", price: 5 },
-  { id: 2, name: "Margherita Pizza", category: "Main Course", price: 10 },
-  { id: 3, name: "Pasta Alfredo", category: "Main Course", price: 12 },
-  { id: 4, name: "Chocolate Cake", category: "Desserts", price: 7 }
-];
