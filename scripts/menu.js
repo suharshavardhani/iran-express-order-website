f5ef96ff6975809af93d83f0ad823a30eeb44119
suharshavardhani <34936342+suharshavardhani@users.noreply.github.com>
2024-10-11 22:55:41 -0700

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,7 @@
 const menu = [
-  { id: 1, name: "Garlic Bread", category: "Starters", price: 5 },
-  { id: 2, name: "Margherita Pizza", category: "Main Course", price: 10 },
-  { id: 3, name: "Pasta Alfredo", category: "Main Course", price: 12 },
-  { id: 4, name: "Chocolate Cake", category: "Desserts", price: 7 }
+  { id: 1, name: "Irani Chai", category: "Beverage", price: 25, img: "https://i.postimg.cc/TPDTQbXR/chai.png" },
+  { id: 2, name: "Charminar Biscuit (3/plate) ", category: "Snacks", price: 10, img: "https://i.postimg.cc/9MPCHjMK/buscuit.png" },
+  { id: 3, name: "Bun Maska (1/plate)", category: "Snacks", price: 50, img: "https://i.postimg.cc/brz8zFSJ/bunmaska.png" },
+  { id: 4, name: "Bread Omlette", category: "Snacks", price: 79, img: "https://i.postimg.cc/zBY8QfJT/breadomlette.png" },
+  { id: 5, name: "Maggie", category: "Sancks", price: 45, img: "https://i.postimg.cc/ZnVm0F6L/maggie.png" }
 ];
